9c321f44b142204d80d5d366d144d2f3b4c7ec0c
cyf <cuiyunfan@sjtu.edu.cn>
2026-02-02 01:11:21 +0800

    update

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from config import Config
+from jidong.backend.config import Config
 from flask import Flask
 from flask_apscheduler import APScheduler
 
