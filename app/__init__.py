fffe46bfce459d53653d1657b9b7eee017353459
cyf <cuiyunfan@sjtu.edu.cn>
2026-02-02 01:44:13 +0800

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
-from . import Config
+from config import Config
 from flask import Flask
 from flask_apscheduler import APScheduler
 
